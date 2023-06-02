d74e773fdf0c1efc2b5f4585578c562fb85327ec
Enzo Victor Stanganelli Gama <stanganelli899@gmail.com>
2023-06-02 10:44:41 -0300

    agora o alerta de umidade funcionando

--- site institucional sprint 3/web-data-viz-main/site/dat-acqu-ino-main/main.js
+++ site institucional sprint 3/web-data-viz-main/site/dat-acqu-ino-main/main.js
@@ -34,7 +34,7 @@ const serial = async (
                 // CREDENCIAIS DO BANCO - MYSQL WORKBENCH
                 host: 'localhost',
                 user: 'root',
-                password: 'sptech',
+                password: 'enzo123',
                 database: 'fungustech'
             }
         ).promise();
